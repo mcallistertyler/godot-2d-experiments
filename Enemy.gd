916a61afd6941edc6efc35f629cd74d06637e89b
Tyler McAllister <mcallistertyler95@gmail.com>
2021-07-17 22:50:10 +0200

    finished very basic logic for reading json to decide which bullets to fire and where

--- Enemy.gd
+++ Enemy.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 const projectile = preload("res://Bullet.tscn")
-
+var bullet_pool = read_bullet_file()
+var bullet_rng = RandomNumberGenerator.new()
 var test_counter = 0
 var test_counter2 = 0
 var valid_bullets = []
@@ -31,20 +32,26 @@ func read_bullet_file():
 	bullet_file.open("res://enemy-test-bullet.json", File.READ)
 	var bullet_content = bullet_file.get_as_text()
 	bullet_file.close()
-	var p_bullet = JSON.parse(bullet_content).result
-	for bullet in p_bullet["bullet_pool"]:
-		print(bullet)
+	return JSON.parse(bullet_content).result
+	
+func generate_bullet():
+	var available_bullets = bullet_pool['bullet_pool']
+	bullet_rng.randomize()
+	var ran_val = bullet_rng.randi_range(0,len(available_bullets) - 1)
+	var bullet_to_fire = projectile.instance()
+	bullet_to_fire.set("cardinal_direction", available_bullets[ran_val].direction)
+	bullet_to_fire.set("speed", available_bullets[ran_val].speed)
+	add_child(bullet_to_fire)
+	
 	
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update()
-	read_bullet_file()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	test_counter += 1
 	if test_counter % 60 == 0:
-		var bullet = projectile.instance()
-		bullet.set("cardinal_direction", "S")
-		add_child(bullet)
+		generate_bullet()
